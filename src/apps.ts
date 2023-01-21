65e07dbd176c191526cb0ab2c4da69bb3c955f9c
adriancelistorres <adriancelistorres184@gmail.com>
2023-01-20 21:28:14 -0500

    se agrego las variables de entorno y la libreria dotenv

--- src/apps.ts
+++ src/apps.ts
@@ -3,6 +3,7 @@ import morgan from "morgan"
 import cors from "cors"
 import userRoutes from "./routes/user.routes"
 
+
 const app=express();
 app.use(morgan('dev'));
 app.use(cors());
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import "reflect-metadata"
 import app from "./apps"
 import { AppDataSource } from "./databases/db"
+require('dotenv').config();
+const PORT=process.env.PORT
 
 async function main(){
     try {
         await AppDataSource.initialize();
         console.log("database conected");
-        app.listen(3000)
-        console.log("server is listening on port",3000)
+        app.listen(PORT)
+        console.log("server is listening on port",PORT)
     } catch (error) {
         console.log(error);
     }
