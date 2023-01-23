5a422b73f8891a9b6b74d4e459d68823fc1a1d1f
adriancelistorres <adriancelistorres184@gmail.com>
2023-01-23 12:06:40 -0500

    nuevos cambios

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { IPerson } from "../Interfaces/IPerson";
 import PersonService from "../services/person.service";
 import bcrypt from 'bcrypt'
+import { AppDataSource } from "../databases/db";
+import Person from "../entities/Person";
 
 class PersonController{
     public async addPerson(req:Request,res:Response){
@@ -10,6 +12,23 @@ class PersonController{
             const reqBody:IPerson=req.body;
             const service=new PersonService();
             const result=await service.addPerson(reqBody);
+            // const username = reqBody.username;
+            // const data = await AppDataSource.createQueryBuilder()
+            //   .select("person")
+            //   .from(Person, "person")
+            //   .where("person.username = :username", { username })
+            //   // .andWhere("person.password = :password", { password })
+            //   .getOne();
+      
+            // if (data?.username !==result.username) {
+            //     console.log('log1');
+            //      return res.jsonp('usuario existe ya')
+            // } else {  
+            //     console.log('log2');
+
+            //      return res.json(result)
+            // }
+        
             return res.json(result)
 
         } catch (error) {
--- src/services/person.service.ts
+++ src/services/person.service.ts
@@ -1,28 +1,29 @@
 import { IPerson } from "../Interfaces/IPerson";
+import { AppDataSource } from "../databases/db";
 import Person from "../entities/Person";
-import bcrypt from 'bcrypt'
-
+import bcrypt from "bcrypt";
 
 class PersonService {
-
   public async addPerson(reqBody: IPerson) {
-
-    const hashedPassword=await bcrypt.hash(reqBody.password,8)
-
-    const person = new Person();
-    person.idrol = reqBody.idrol;
-    person.name = reqBody.name;
-    person.lastname = reqBody.lastname;
-    person.date_b = reqBody.date_b;
-    person.dni = reqBody.dni;
-    person.gender = reqBody.gender;
-    person.address = reqBody.address;
-    person.username = reqBody.username;
-    person.password = hashedPassword;
-    person.state = reqBody.state;
-    person.key = reqBody.key;
-
-    return await person.save();
+    try {
+      const hashedPassword = await bcrypt.hash(reqBody.password, 8);
+      const person = new Person();
+      person.idrol = reqBody.idrol;
+      person.name = reqBody.name;
+      person.lastname = reqBody.lastname;
+      person.date_b = reqBody.date_b;
+      person.dni = reqBody.dni;
+      person.gender = reqBody.gender;
+      person.address = reqBody.address;
+      person.username = reqBody.username;
+      person.password = hashedPassword;
+      person.state = reqBody.state;
+      person.key = reqBody.key;
+      return await person.save();
+    
+    } catch (error) {
+      return Promise.reject(" does not exist ");
+    }
   }
 }
 export default PersonService;
