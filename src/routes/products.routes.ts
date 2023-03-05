aab0d5618ac5a39fe601fcc17601bf6449466928
DyerMandujano <dyer0206@gmail.com>
2023-03-04 19:32:21 -0500

    Agregando rutas para cliente

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -7,10 +7,15 @@ const validatetoken =  new ValidateToken;
 
 const router =  Router();
 
+//RUTAS PARA ADMIN
 router.post("/addproduct",validatetoken.Token,productsController.addProduct);
 router.get("/products",validatetoken.Token,productsController.getProducts);
 router.get("/product/:idproduc",validatetoken.Token,productsController.getOneProduct);
 router.put("/product/:idproduc",validatetoken.Token,productsController.UpdateProduct);
 router.delete("/product/:idproduc",validatetoken.Token,productsController.deleteProduct);
+
+//RUTAS PARA EL CLIENTE
+router.get("/products2",productsController.getProducts);
+router.get("/product2/:idproduc",productsController.getOneProduct);
 
 export default router;
